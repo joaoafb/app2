61c620809b6003e86db46f3e520d1718cf986483
joaoafb <82537300+joaoafb@users.noreply.github.com>
2021-12-12 13:53:23 -0300

    Add files via upload

---
+++ chaterrolinks/app.js
@@ -0,0 +1,23 @@
+let myName = localStorage.getItem("nome")
+
+
+
+const sendMessage = () => {
+
+
+    let message = document.getElementById("message").value
+    firebase.database().ref("msgerrolinks").push().set({
+        "sender": myName,
+        "message": message
+    });
+    let message1 = document.getElementById("message")
+    message1.value = ""
+    document.querySelector("#chatbox").scrollTop = document.querySelector("#chatbox").scrollHeight
+
+
+
+
+
+
+    return false;
+}
